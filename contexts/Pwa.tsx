184c1be2f5513d7e7800486c7801e252bb2e17be
matheus correa <mathec@live.com>
2022-07-11 09:59:43 -0300

    feat: configuration pwa

--- contexts/Pwa.tsx
+++ contexts/Pwa.tsx
@@ -6,7 +6,20 @@ import LinearProgress from '@mui/material/LinearProgress';
 const Pwa: React.FC<{children: React.ReactNode}> = (props) => {
   return (
     <PwaApp
-      config={{ swUrl: '/sw.js' }}
+      config={{ 
+        swUrl: '/sw.js',
+        onUpdate: () => {
+          alert('new updates available, please, close the app to finish installation')
+        },
+        onPrompt: (p) => {
+          if(p.outcome === 'accepted'){
+            setTimeout(() => {
+              alert('app installed, close the browser and open the mobile menu')
+              window.close()
+            }, 10000)
+          }
+        }
+      }}
       suspense={<Box> <LinearProgress /></Box>}
       {...props}
     />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,7 @@ const App = ({ Component, pageProps }: AppProps) => {
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.webmanifest" />
       </Head>
       <Theme>
